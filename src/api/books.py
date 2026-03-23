8e6aadc29ec1d9c4cf128c28ce5c07946d85992a
two4faced <jyna5li@gmail.com>
2026-03-23 13:22:02 +0300

    create POST method for books

--- src/api/books.py
+++ src/api/books.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, Depends
 
-from src.api.dependencies import get_db
+from src.api.dependencies import get_db, get_user_id
 from src.database.db_manager import DBManager
+from src.schemas.books import AddBookRequestDTO, AddBookDTO
 
 router = APIRouter(prefix='/books', tags=['books'])
 
@@ -14,3 +15,16 @@ async def get_all_books(db: DBManager = Depends(get_db)):
 @router.get('/{book_id}')
 async def get_one_book(book_id: int, db: DBManager = Depends(get_db)):
     return await db.books.get_one_or_none(id=book_id)
+
+
+@router.post('')
+async def add_book(
+    book_data: AddBookRequestDTO,
+    db: DBManager = Depends(get_db),
+    user_id: int = Depends(get_user_id),
+):
+    new_book_data = AddBookDTO(author_id=user_id, **book_data.model_dump())
+    new_book = await db.books.add(new_book_data)
+
+    await db.commit()
+    return new_book
