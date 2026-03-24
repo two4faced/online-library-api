4a7dec63e50262f08ad88c2a4b1ea3924395f0e3
two4faced <jyna5li@gmail.com>
2026-03-24 11:09:21 +0300

    create DELETE and PATCH methods for books, add PatchBookDTO

--- src/api/books.py
+++ src/api/books.py
@@ -2,22 +2,22 @@ from fastapi import APIRouter, Depends
 
 from src.api.dependencies import get_db, get_user_id
 from src.database.db_manager import DBManager
-from src.schemas.books import AddBookRequestDTO, AddBookDTO
+from src.schemas.books import AddBookRequestDTO, AddBookDTO, PatchBookDTO
 
 router = APIRouter(prefix='/books', tags=['books'])
 
 
-@router.get('')
+@router.get('', summary='get all books')
 async def get_all_books(db: DBManager = Depends(get_db)):
     return await db.books.get_all()
 
 
-@router.get('/{book_id}')
+@router.get('/{book_id}', summary='get one book by id')
 async def get_one_book(book_id: int, db: DBManager = Depends(get_db)):
     return await db.books.get_one_or_none(id=book_id)
 
 
-@router.post('')
+@router.post('', summary='add new book')
 async def add_book(
     book_data: AddBookRequestDTO,
     db: DBManager = Depends(get_db),
@@ -28,3 +28,17 @@ async def add_book(
 
     await db.commit()
     return new_book
+
+
+@router.delete('{book_id}', summary='delete book')
+async def delete_book(book_id: int, db: DBManager = Depends(get_db)):
+    await db.books.delete(id=book_id)
+    await db.commit()
+    return {'status': 200}
+
+
+@router.patch('{book_id}', summary='partially change book')
+async def change_book(book_data: PatchBookDTO, book_id: int, db: DBManager = Depends(get_db)):
+    await db.books.change(book_data, exclude_unset=True, id=book_id)
+    await db.commit()
+    return {'status': 200}
--- src/schemas/books.py
+++ src/schemas/books.py
@@ -5,11 +5,11 @@ class AddBookRequestDTO(BaseModel):
     title: str = Field(max_length=90)
     description: str = Field(max_length=2000)
     volume: int = Field(le=1500)
-    rating: float = Field(default=0.0, ge=0, le=10)
 
 
 class AddBookDTO(AddBookRequestDTO):
     author_id: int | None
+    rating: float = Field(default=0.0, ge=0, le=10)
 
 
 class BookDTO(BaseModel):
@@ -19,3 +19,10 @@ class BookDTO(BaseModel):
     description: str = Field(max_length=2000)
     volume: int = Field(le=1500)
     rating: float = Field(default=0.0, ge=0, le=10)
+
+
+class PatchBookDTO(BaseModel):
+    title: str | None = Field(default=None, max_length=90)
+    description: str | None = Field(default=None, max_length=2000)
+    volume: int | None = Field(default=None, le=1500)
+    rating: float | None = Field(default=None, ge=0, le=10)
